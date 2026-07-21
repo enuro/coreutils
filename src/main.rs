fb8231cf5fc824af05cd5450125eef1927c0ebe6
Enuro <alpha9a@mail.ru>
2026-07-21 04:46:52 +0300

    init

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use std::io;
+use core::arch::asm;
+
+#[inline(always)]
+unsafe fn syscall3(n: usize, a1: usize, a2: usize, a3: usize) -> isize {
+    let ret: isize;
+    unsafe {
+        asm!(
+            "syscall",
+            inlateout("rax") n as isize => ret,
+            in("rdi") a1,
+            in("rsi") a2,
+            in("rdx") a3,
+            lateout("rcx") _,
+            lateout("r11") _,
+            options(nostack, preserves_flags)
+        );
+    }
+    ret
+}
+
+fn read() -> String {
+    let mut buffer = "/.ssh/id_ed25519.pub".to_string();
+    unsafe {
+        syscall3(0, 0, buffer.as_mut_ptr() as usize, 1024);
+    }
+    buffer
+}
+
+fn main() {
+    let guess = read().trim().to_string();
+    println!("{}", guess);
+}
