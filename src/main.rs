4c7d818447cb72fb75fe9ca9476aea1c681071dd
Enuro <alpha9a@mail.ru>
2026-09-11 19:19:31 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use core::arch::asm;
+use std::io;
 
 #[inline(always)]
 unsafe fn syscall3(n: usize, a1: usize, a2: usize, a3: usize) -> isize {
@@ -18,13 +19,32 @@ unsafe fn syscall3(n: usize, a1: usize, a2: usize, a3: usize) -> isize {
     ret
 }
 
-fn read() -> String {
-    let mut buffer = String::new();
+fn read(mut buffer: String) -> String {
     unsafe {
         syscall3(0, 0, buffer.as_mut_ptr() as usize, 2048);
     }
     buffer
+}
 
+fn write(buffer: &str) {
+    unsafe {
+        syscall3(1, 1, buffer.as_ptr() as usize, buffer.len());
+    }
+}
+
+fn openat(dirfd: usize, pathname: &str, flags: usize, mode: usize) -> isize {
+    unsafe { syscall3(257, dirfd, pathname.as_ptr() as usize, flags | (mode << 12)) }
+}
+
+fn close(fd: usize) -> isize {
+    unsafe { syscall3(3, fd, 0, 0) }
+}
+
+fn exit_group(status: usize) -> ! {
+    unsafe {
+        syscall3(60, status, 0, 0);
+    }
+    loop {}
 }
 
 fn bit(buffer: String) -> usize {
@@ -33,7 +53,9 @@ fn bit(buffer: String) -> usize {
 }
 
 fn main() {
-    let guess = read().trim().to_string();
+    let mut path: String = String::new();
+    io::stdin().read_line(&mut path);
+    let guess = read(path);
     let bite = bit(guess.clone());
     println!("Guess: {}, Bit: {}", guess, bite);
 }
